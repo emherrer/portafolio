914b8d3b6c0f656cdf905f90cc2935e5dd06c82b
Emmanuel Herrera <emherrer1@gmail.com>
2023-06-27 21:38:54 -0400

    render the titles in two columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -26,3 +27,15 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for idx, row in df[:10].iterrows():
+        st.title(row["title"])
+
+with col4:
+    for idx, row in df[10:].iterrows():
+        st.title(row["title"])
