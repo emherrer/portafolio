b433134203dafb5bbc9de486c4679cbf2926620d
Emmanuel Herrera <emherrer1@gmail.com>
2023-06-29 00:54:16 -0400

    add password in send_email

--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "emherrer1@gmail.com"
-    password = "wznwyaghaohfqlfg"
+    password = os.getenv("PASSWORD")
 
     receiver = "emherrer1@gmail.com"
     context = ssl.create_default_context()
